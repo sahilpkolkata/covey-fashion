defeacfaeea6edd593f6702ae258301802fb199d
unknown <sahilpkolkata@gmail.com>
2020-12-21 00:07:47 +0530

    Added shop page

---
+++ covey-fashion/src/pages/shop/shop.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './shop.styles.scss'
+import SHOP_DATA from './shop.data'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+class ShopPage extends React.Component{
+    constructor(props){
+        super()
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render(){
+        return(
+            <div className='shop-page'>
+        
+                   { this.state.collections.map(({id,...otherProps})=>(
+                        <CollectionPreview key={id} {...otherProps} />
+                    ))
+            }
+           </div>
+        )
+    }
+}
+
+export default ShopPage
