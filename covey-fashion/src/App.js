7e51be9a9d633305ca6cb5600a8ddc23f7e3576a
unknown <sahilpkolkata@gmail.com>
2021-01-06 09:10:03 +0530

    Added redirecting user auth to homepage

--- covey-fashion/src/App.js
+++ covey-fashion/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
@@ -45,7 +45,11 @@ class App extends React.Component{
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
-            <Route path='/signin' component={AuthPage} />
+            <Route exact path='/signin' 
+              render={()=> this.props.currentUser ?
+                          (<Redirect to='/' />) :
+                          (<AuthPage />)
+            } />
         </Switch>
       </div>
     );
@@ -53,8 +57,12 @@ class App extends React.Component{
   
 }
 
+const mapStateToProps = ({user})=>({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch=>({
     setCurrentUser: user=> dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
