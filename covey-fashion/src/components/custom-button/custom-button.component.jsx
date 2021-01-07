4dd61d042ac616babf1a83a4bafff47077bbb728
unknown <sahilpkolkata@gmail.com>
2021-01-07 19:17:36 +0530

    Added cart utils and updated shop data

--- covey-fashion/src/components/custom-button/custom-button.component.jsx
+++ covey-fashion/src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './custom-button.styles.scss'
 
-const CustomButton = ({children, isGoogle, ...otherProps})=>(
-    <button className={`${isGoogle ? 'google-signin' : ''} custom-button`} {...otherProps}>{children}</button>
+const CustomButton = ({children, isGoogle, inverted, ...otherProps})=>(
+    <button className={`${inverted ? 'inverted' : ''}${isGoogle ? 'google-signin' : ''} custom-button`} {...otherProps}>{children}</button>
 )
 
 export default CustomButton
